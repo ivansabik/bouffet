70903e16c3f1c72eeb5529ea0b2c39b7473f4ebc
Ivan Rodriguez <ivan@solofunds.com>
2023-10-28 11:38:51 -0600

    Add missing PK for models

--- foodeliverty_api/tenants/models.py
+++ foodeliverty_api/tenants/models.py
@@ -1,9 +1,13 @@
+import uuid
+
 from django.db import models
 from tenant_schemas.models import TenantMixin
 
 
 class Client(TenantMixin):
+    auto_create_schema = True
     created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     on_trial = models.BooleanField()
     paid_until = models.DateField(null=True, blank=True)
